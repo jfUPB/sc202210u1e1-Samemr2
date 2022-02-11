22e7e175c97de4a39dd3d581f0bbae7016fc1f55
samu <samuel.morales@upb.edu.co>
2022-02-11 00:31:22 -0500

    instanciacion de variables

--- problem/program.asm
+++ problem/program.asm
@@ -2,5 +2,25 @@
 // 000431580
 // samuel.morales@upb.edu.co
 (START)
-    @START
+    @67
+	D=A
+	@c
+	//cogigo c
+	M=D
+	@70
+	D=A
+	@f
+	//codigo F
+	M=D
+	@24576
+	D=A 
+	@k
+	//input keyboard
+	M=D 
+	@24575
+	D=A 
+	@indice_pixel
+	M=D
+	@fill
+	M=M
     0;JMP
